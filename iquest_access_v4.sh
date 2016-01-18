abdc3acdb279080018c8463f9303da40222bcb13
Jäälinoja Harri Tapio <hajaalin@dhcp-asv-66.helsinki.fi>
2016-01-18 13:12:59 +0200

    autoindent in atom

--- iquest_access_v4.sh
+++ iquest_access_v4.sh
@@ -11,7 +11,7 @@ function debug() {
 
 if [[ -z "$1" ]] || [[ -z "$2" ]]
 then
-    exit 1
+  exit 1
 fi
 
 # we will check the permissions of data in this collection
@@ -30,16 +30,16 @@ declare -a groups
 debug "coll_users"
 while read line
 do
-    user_name=`echo "$line" | awk -F"###" '{ print $1 }'`
-    user_id=`echo "$line" | awk -F "###" '{ print $2 }'`
-    debug $user_name
-    if [[ "$user_name" == *.* ]] || [[ "$user_name" == "rods" ]] || [[ "$user_name" == "rodsadmin" ]]
-    then
-      debug "skipping $user_name"
-      continue
-    else
-        user_name_id_map+=( ["$user_id"]="$user_name" )
-    fi
+  user_name=`echo "$line" | awk -F"###" '{ print $1 }'`
+  user_id=`echo "$line" | awk -F "###" '{ print $2 }'`
+  debug $user_name
+  if [[ "$user_name" == *.* ]] || [[ "$user_name" == "rods" ]] || [[ "$user_name" == "rodsadmin" ]]
+  then
+    debug "skipping $user_name"
+    continue
+  else
+    user_name_id_map+=( ["$user_id"]="$user_name" )
+  fi
 done <<< "$coll_users"
 
 # read the id of the LMU Ida project group
@@ -51,47 +51,47 @@ group_id=`iquest --no-page "%s" "SELECT USER_ID WHERE USER_NAME = 'hy.hy7004'"`
 debug "colls"
 while read line
 do
-    debug $line
+  debug $line
 
-    # check what access group hy.hy7004 has to the collection
-    coll_access_info=`iquest --no-page "%s###%s" "SELECT COLL_ACCESS_USER_ID, COLL_ACCESS_NAME WHERE COLL_NAME = '${line}' AND COLL_ACCESS_USER_ID = '${group_id}'" 2> /dev/null`
-    coll_access_string=""
+  # check what access group hy.hy7004 has to the collection
+  coll_access_info=`iquest --no-page "%s###%s" "SELECT COLL_ACCESS_USER_ID, COLL_ACCESS_NAME WHERE COLL_NAME = '${line}' AND COLL_ACCESS_USER_ID = '${group_id}'" 2> /dev/null`
+  coll_access_string=""
 
-    # this is the case when hy.hy7004 doesn't have access to the collection
-    if [[ "$?" != "0" ]] || [[ "$coll_access_info" == *"CAT"* ]]
-    then
+  # this is the case when hy.hy7004 doesn't have access to the collection
+  if [[ "$?" != "0" ]] || [[ "$coll_access_info" == *"CAT"* ]]
+  then
 
-      # check if LMU staff members have access
-      readable_by_lmu="true"
-      for uid in "${!user_name_id_map[@]}"
-      do
-          user_name="${user_name_id_map["$uid"]}"
+    # check if LMU staff members have access
+    readable_by_lmu="true"
+    for uid in "${!user_name_id_map[@]}"
+    do
+      user_name="${user_name_id_map["$uid"]}"
 
-          coll_access_info=`iquest --no-page "%s###%s" "SELECT COLL_ACCESS_USER_ID, COLL_ACCESS_NAME WHERE COLL_NAME = '${line}' AND COLL_ACCESS_USER_ID = '${uid}'" 2> /dev/null`
-          if [[ "$?" != "0" ]] || [[ "$coll_access_info" == *"CAT"* ]]
-          then
-            readable_by_lmu="false"
-            access_name="null"
-          else
-            access_name=`echo "$coll_access_info" | awk -F"###" '{ print $2 }'`
-          fi
-
-          debug "$user_name $access_name $coll_access_info"
-          coll_access_string+="${user_name_id_map["$uid"]}:${access_name} "
-          if [[ "$access_name" == "null" ]]
-          then
-            readable_by_lmu="false"
-          fi
-      done
-
-      if [[ "$readable_by_lmu" == "false" ]]
+      coll_access_info=`iquest --no-page "%s###%s" "SELECT COLL_ACCESS_USER_ID, COLL_ACCESS_NAME WHERE COLL_NAME = '${line}' AND COLL_ACCESS_USER_ID = '${uid}'" 2> /dev/null`
+      if [[ "$?" != "0" ]] || [[ "$coll_access_info" == *"CAT"* ]]
       then
-        echo "ALERT: ${line} not readable by LMU staff. Permissions: ${coll_access_string}"
+        readable_by_lmu="false"
+        access_name="null"
       else
-        echo "${line} LMU staff has access: ${coll_access_string}"
+        access_name=`echo "$coll_access_info" | awk -F"###" '{ print $2 }'`
       fi
 
+      debug "$user_name $access_name $coll_access_info"
+      coll_access_string+="${user_name_id_map["$uid"]}:${access_name} "
+      if [[ "$access_name" == "null" ]]
+      then
+        readable_by_lmu="false"
+      fi
+    done
+
+    if [[ "$readable_by_lmu" == "false" ]]
+    then
+      echo "ALERT: ${line} not readable by LMU staff. Permissions: ${coll_access_string}"
     else
-      echo "${line} hy.hy7004 has access: ${coll_access_info}"
+      echo "${line} LMU staff has access: ${coll_access_string}"
     fi
+
+  else
+    echo "${line} hy.hy7004 has access: ${coll_access_info}"
+  fi
 done <<< "$colls"
